34f9e8388664a18df5597027ace13568621ace25
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-09-29 13:37:07 -0400

    docstrings

--- Labs/Lab3/q3b.py
+++ Labs/Lab3/q3b.py
@@ -1,10 +1,14 @@
 def is_divisible(n, m):
+    '''(integer, integer) -> (boolean)
+    returns if n is divisible by m'''
     return n%m == 0
 
 def is_divisible23n8(x):
-	if((x%2 == 0 or x%3 == 0) and x%8 != 0):
-		return 'yes'
-	return 'no'
+    '''(integer) -> (string)
+    returns 'yes' if x is divisible by 2 or 3 but not by 8, 'no' otherwise'''
+    if((x%2 == 0 or x%3 == 0) and x%8 != 0):
+	return 'yes'
+    return 'no'
 
 x = int(input("Enter an integer: "))
 if(is_divisible23n8(x) == 'yes'):
