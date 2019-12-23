11b59985f0fffcd81449203c445ebda0d03a04be
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-12-22 20:53:09 -0500

    doc

--- Labs/Lab6/prog-ex-1.py
+++ Labs/Lab6/prog-ex-1.py
@@ -1,4 +1,6 @@
 def sum_odd_while_v2(n):
+    '''(integer) -> (integer)
+    computes the sum of all integers between 5 and n inclusively'''
     sum = 0
     for i in range(5, n+1, 2):
         sum += i
