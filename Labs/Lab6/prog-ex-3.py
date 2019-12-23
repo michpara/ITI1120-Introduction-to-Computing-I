3a13ac42d726bb2df2899a53140a2e16a0b9f348
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-12-22 20:55:19 -0500

    doc

--- Labs/Lab6/prog-ex-3.py
+++ Labs/Lab6/prog-ex-3.py
@@ -1,4 +1,8 @@
 def first_neg(nums):
+    '''(list)->(integer)
+    returns the first index of a negative number'''
+    if len(nums) == 0:
+        return None
     for i in nums:
         if i < 0:
             return nums.index(i)
