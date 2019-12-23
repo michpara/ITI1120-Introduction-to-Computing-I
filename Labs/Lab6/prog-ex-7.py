943e6b19e61d86d4040e2bda8cffe18cee3b79be
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-12-22 20:58:55 -0500

    doc

--- Labs/Lab6/prog-ex-7.py
+++ Labs/Lab6/prog-ex-7.py
@@ -1,4 +1,6 @@
 def inner_product(listA, listB):
+    '''(list, list)->(integer)
+    returns the inner product of listA and listB'''
     sum = 0
     for i in range(0, len(listA)):
         product = listA[i]*listB[i]
